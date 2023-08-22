53fd603a2e05442ee9ce9e856846e622d3326bc3
Zhou ZJ <43747306+ZhijianZhou01@users.noreply.github.com>
2023-08-22 22:09:51 +0800

    Update __init__.py

--- ervdetective/__init__.py
+++ ervdetective/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
 
+"""
+Author: Zhou Zhi-Jian
+Email: zjzhou@hnu.edu.cn
+Time: 2021/11/9 15:16
+
+"""
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
