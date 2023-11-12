79abc0ae578eb86a50ab816c9dbe4159a5fb571c
Zhou ZJ <43747306+ZhijianZhou01@users.noreply.github.com>
2023-11-12 23:09:22 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding="utf-8") as fh:
 
 setuptools.setup(
   name="ervdetective",
-  version="0.5.1",
+  version="0.5.2",
   author="Zhi-Jian Zhou",
   author_email="zjzhou@hnu.edu.cn",
   description="An automated pipeline for identification and annotation of endogenous retroviruses (ERVs)",
